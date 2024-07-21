0f5ada8ce19b69ab2141843ad1ab56d799019660
XYCode Kerman <xycode-xyc@outlook.com>
2024-07-21 15:12:24 +0800

    :memo:(yiriob/) 增加一些docstring

--- yiriob/bot.py
+++ yiriob/bot.py
@@ -6,11 +6,56 @@ import pydantic
 from yiriob.adapters.base import Adapter
 from yiriob.event.base import (EventBase, events_list, get_key_from_dict,
                                get_key_from_event)
+from yiriob.interface.message import (SendGroupMessageInterface,
+                                      SendGroupMessageParams,
+                                      SendGroupMessageResponse,
+                                      SendPrivateMessageInterface,
+                                      SendPrivateMessageParams,
+                                      SendPrivateMessageResponse)
+from yiriob.message.message_chain import MessageChain
 from yiriob.utils import logger
 
 
 class Bot:
     def __init__(self, adapter: Adapter, self_id: int) -> None:
+        """机器人类。
+
+        Args:
+            adapter: 适配器，用于跟 OneBot 实现通讯
+            self_id: 机器人 id，避免接收到其它机器人的事件
+
+        你可以像这样初始化一个机器人：
+
+        ```python
+        bus = EventBus()
+        bot = Bot(
+            adapter=ReverseWebsocketAdapter(
+                host="127.0.0.1", port=8080, access_token="...", bus=bus
+            ),
+            self_id=...,
+        )
+        ```
+
+        然后像这样监听事件并处理事件（还有调用 API）：
+
+        ```python
+        @bus.on(GroupMessageEvent)
+        async def on_group_message(event: GroupMessageEvent) -> None:
+            resp = await bot.adapter.call_api(
+                SendGroupMessageInterface,
+                SendGroupMessageParams(
+                    group_id=event.group_id, message=MessageChain([Text("Hello World!")])
+                ),
+            )
+        ```
+
+        一些常见的 API 有简化形式，例如发送消息：
+
+        ```python
+        await bot.send_group_message(group_id=..., message=...)
+        await bot.send_private_message(user_id=..., message=...)
+        ```
+        """
         self.adapter = adapter
         self.id = self_id
         self.bus = adapter.bus
@@ -18,6 +63,11 @@ class Bot:
         self.bus.subscribe("onebot_event", self._onebot_event_handler)
 
     async def _onebot_event_handler(self, data: dict[str, Any]) -> None:
+        """将原始的onebot事件解析为事件类并触发事件总线。
+
+        Args:
+            data: 原始事件
+        """
         validated_model: Optional[EventBase] = next(
             (
                 x(**data)
@@ -48,6 +98,44 @@ class Bot:
         logger.debug(f"匹配到事件：{type(validated_model)}")
         self.bus.emit(type(validated_model), validated_model)
 
+    async def send_group_message(
+        self,
+        group_id: int,
+        message: MessageChain | str,
+        auto_escape: bool = True,
+    ) -> SendGroupMessageResponse:
+        """发送群消息。
+
+        Args:
+            group_id: 群号
+            message: 消息
+            auto_escape: 是否自动转义
+        """
+        return await self.adapter.call_api(
+            SendGroupMessageInterface,
+            SendGroupMessageParams(
+                group_id=group_id, message=message, auto_escape=auto_escape
+            ),
+        )
+
+    async def send_private_message(
+        self, user_id: int, message: MessageChain | str, auto_escape: bool = True
+    ) -> SendPrivateMessageResponse:
+        """发送私聊消息。
+
+        Args:
+            user_id: 用户 id
+            message: 消息
+            auto_escape: 是否自动转义
+        """
+        return await self.adapter.call_api(
+            SendPrivateMessageInterface,
+            SendPrivateMessageParams(
+                user_id=user_id, message=message, auto_escape=auto_escape
+            ),
+        )
+
     def run(self) -> None:
+        """启动机器人。该方法将会开启适配器，并且阻塞。"""
         self.adapter.start()
         asyncio.get_event_loop().run_forever()
--- yiriob/message/message_chain.py
+++ yiriob/message/message_chain.py
@@ -7,6 +7,75 @@ from .message_components import MessageComponent, Text
 
 
 class MessageChain(list[MessageComponent]):
+    """消息链。
+
+    构造消息链的方法：
+
+    ```python
+    chain = MessageChain([
+        Text("hello"),
+        Image(...)
+    ])
+    ```
+
+    你也可以把 Text 省略，下面这种方法和上面是等价的：
+
+    ```python
+    chain = MessageChain([
+        "hello",
+        Image(...)
+    ])
+    ```
+
+    你可以传入任何一个**可迭代**的对象，比如传入一个生成器：
+
+    ```python
+    def generator():
+        # ...
+        yield ...
+        # ...
+
+    chain = MessageChain(generator)
+    ```
+
+    ```python
+    chain = MessageChain((x for x in ... if ...))  # 这是 Python 的生成器推导式写法
+    ```
+
+    使用 to_cqcode 方法将其转换为 CQ 码（一种用字符串表示消息链的方式）：
+
+    ```python
+    chain = MessageChain([Text("hello"), Text("world"), Face(id=1)])
+    chain.to_cqcode()
+
+    # > "Helloworld[CQ:face,id=1]"
+    ```
+
+    如果需要自定义消息组件（OneBot 里叫 MessageSegment，这里沿用 Mirai 的称呼），需要继承 `yiriob.message.message_components.MessageComponent`，然后指定 `comp_type` 类型：
+
+    ```python
+    class CustomComponent(MessageComponent):
+        comp_type: str = "..."
+        # ...
+    ```
+
+    由于 MessageChain 继承了 list[MessageComponent], 你可以像使用 list 一样使用它，比如：
+
+    ```python
+    chain: MessageChain = ...
+
+    for comp in chain:
+        ...
+
+    if comp in chain:
+        ...
+
+    chain.append(comp)
+    chain.extend(comps)
+    len(chain)
+    ```
+    """
+
     def __init__(self, iterable: Iterable[MessageComponent | str], /) -> None:
         self.extend([Text(x) if isinstance(x, str) else x for x in iterable])
 
